328d606a841cb5dcd28763bc318ccbcc7be26023
DaanvdPoel <78739498+DaanvdPoel@users.noreply.github.com>
2022-06-03 08:39:01 +0200

    Add files via upload

--- First Person Puzzler/Assets/Scripts/CheckpointManager.cs
+++ First Person Puzzler/Assets/Scripts/CheckpointManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CheckpointManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    private CharacterController playerController;
     //[SerializeField] private TimerScript timerScript;
 
     //[0] typingPuzzle || [1] electricityFixing || [2] wireconnectingPuzzle || [3]
@@ -12,34 +13,59 @@ public class CheckpointManager : MonoBehaviour
 
     private void Start()
     {
-
+        playerController = player.GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            SaveCheckpoint();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            LoadCheckpoint();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ResetCheckPoint();
+        }
     }
 
     public void SaveCheckpoint()
     {
-        PlayerPrefs.SetFloat("playerPositionX", player.transform.position.x);
-        PlayerPrefs.SetFloat("playerPositionY", player.transform.position.y);
-        PlayerPrefs.SetFloat("playerPositionZ", player.transform.position.z);
+        ResetCheckPoint();
+        Vector3 playerPosition = player.transform.position;
 
+        PlayerPrefs.SetFloat("playerPositionX", playerPosition.x);
+        PlayerPrefs.SetFloat("playerPositionY", playerPosition.y);
+        PlayerPrefs.SetFloat("playerPositionZ", playerPosition.z);
+
+        Debug.Log("save" + playerPosition);
       //    PlayerPrefs.SetFloat("timeRemaining", timerScript.timeRemaining);
     }
 
     public void LoadCheckpoint()
     {
-        PlayerPrefs.GetFloat("playerPositionX", player.transform.position.x);
-        PlayerPrefs.GetFloat("playerPositionY", player.transform.position.y);
-        PlayerPrefs.GetFloat("playerPositionZ", player.transform.position.z);
+        playerController.enabled = false;
+        Vector3 newPlayerPosition;
+
+        newPlayerPosition.x = PlayerPrefs.GetFloat("playerPositionX");
+        newPlayerPosition.y = PlayerPrefs.GetFloat("playerPositionY");
+        newPlayerPosition.z = PlayerPrefs.GetFloat("playerPositionZ");
 
         //  PlayerPrefs.GetFloat("timeRemaining", timerScript.timeRemaining);
+        Debug.Log("load" + newPlayerPosition);
+        player.transform.position = newPlayerPosition;
+        playerController.enabled = true;
     }
 
     public void ResetCheckPoint()
     {
+        Debug.Log("reset");
+
         PlayerPrefs.DeleteKey("playerPositionX");
         PlayerPrefs.DeleteKey("playerPositionY");
         PlayerPrefs.DeleteKey("playerPositionZ");
