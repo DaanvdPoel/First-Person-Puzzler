17cb82d44af44dcebe72af0264ff18749376e0f3
chirp777 <supersonicsparky7@gmail.com>
2022-06-07 16:18:24 +0200

    inrichting

---
+++ First Person Puzzler/Assets/ObjectKnipper.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectKnipper : MonoBehaviour
+{
+    public bool toggle = false;
+    public Vector2 cooldown, duration;
+    float cooldownT, durationT;
+    public GameObject toggledObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTimer();
+    }
+    
+    void ResetTimer() => cooldownT = Random.Range(cooldown.x, cooldown.y);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((cooldownT < Time.deltaTime) && (!(cooldownT == -21)))
+        {
+            //do the thing
+            cooldownT = -21;
+            
+            if (toggle)
+            {
+                toggledObject.SetActive(!toggledObject.active);
+                ResetTimer();
+            }
+            else
+            {
+                durationT = Random.Range(duration.x, duration.y);
+                StartCoroutine(bruh());
+            }
+        }
+        else
+        {
+            cooldownT -= Time.deltaTime;
+        }
+    }
+    
+    IEnumerator bruh()
+    {
+        toggledObject.SetActive(!toggledObject.active);
+        yield return new WaitForSeconds(durationT);
+        toggledObject.SetActive(!toggledObject.active);
+        ResetTimer();
+        
+    }
+}
