4bff43fa5f675706f51f02d21352acc893475be6
DaanvdPoel <200554@student.glu.nl>
2022-06-08 14:24:54 +0200

    .

---
+++ First Person Puzzler/Assets/Scripts/AudioScriptableObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Audiodata", menuName = "ScriptableObjects/Audiodata", order = 0)]
+public class AudioScriptableObject : ScriptableObject
+{
+    public AudioClip[] soundEffects;
+    public AudioClip[] backgroundMusic;
+
+}
+
