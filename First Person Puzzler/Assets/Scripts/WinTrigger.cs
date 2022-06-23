e919387b16d5dccd0658d2d009163f6275a4c892
ginnkorngold <38727595+ginnkorngold@users.noreply.github.com>
2022-06-23 11:45:46 +0200

    End cutscene added

--- First Person Puzzler/Assets/Scripts/WinTrigger.cs
+++ First Person Puzzler/Assets/Scripts/WinTrigger.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinTrigger : MonoBehaviour
 {
-    [SerializeField] private GameObject interactText;
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private GameObject EndCutscene;
+    [SerializeField] private GameObject UI;
+    [SerializeField] private GameObject Present;
+    [SerializeField] private GameObject Past;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject EndsceneCamera;
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            GameManager.instance.PlayerWon();
             characterController.enabled = false;
+            UI.SetActive(false);
+            Present.SetActive(false);
+            Past.SetActive(false);
+            Player.SetActive(false);
+            EndsceneCamera.SetActive(true);
+            EndCutscene.SetActive(true);
         }
+    }
+
+    private void Update()
+    {
+        if (EndCutscene.active == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+
     }
 }
